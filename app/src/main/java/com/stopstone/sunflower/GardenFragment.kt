383126273702d8820a345979ed086b64937aea3b
stopstone <wltjr6432@naver.com>
2024-04-25 21:36:14 +0900

    feat: 초기 화면 셋팅 - 앱바 구성 - 탭 레이아웃 구성 - 탭 별 프래그먼트 전환

---
+++ app/src/main/java/com/stopstone/sunflower/GardenFragment.kt
@@ -0,0 +1,24 @@
+package com.stopstone.sunflower
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+class GardenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_garden, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Toast.makeText(context, this.javaClass.simpleName, Toast.LENGTH_SHORT).show()
+    }
+}
