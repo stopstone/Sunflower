6c7a431691df645a929b6dfa52371eb19653f8ce
stopstone <wltjr6432@naver.com>
2024-04-29 15:56:50 +0900

    refactor: 이미지로드를 백그라운드에서 처리

--- app/src/main/java/com/stopstone/sunflower/extention/ImageExtension.kt
+++ app/src/main/java/com/stopstone/sunflower/extention/ImageExtension.kt
@@ -5,10 +5,17 @@ import android.graphics.BitmapFactory
 import android.widget.ImageView
 
 fun ImageView.load(imageUrl: Int?) {
-    val bitmap = imageUrl?.let { BitmapFactory.decodeResource(resources, it) }
-    val scaledBitmap = bitmap?.let { Bitmap.createScaledBitmap(it, WIDTH, HEIGHT, false) }
+    Thread {
+        // 백그라운드 스레드에서 비트맵 처리 작업 실행
+        val bitmap = imageUrl?.let { BitmapFactory.decodeResource(resources, it) }
+        val scaledBitmap = bitmap?.let { Bitmap.createScaledBitmap(it, WIDTH, HEIGHT, false) }
 
-    scaledBitmap?.let { this.setImageBitmap(it) }
+        // UI 스레드에서 이미지뷰에 비트맵 설정
+        post {
+            scaledBitmap?.let { this.setImageBitmap(it) }
+        }
+    }.start()
+
 }
 
 const val WIDTH = 150
