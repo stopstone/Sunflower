5b6c27018a145a9b8d75299d83bb59cff9fbe1c8
stopstone <wltjr6432@naver.com>
2024-06-09 17:31:50 +0900

    클릭 이벤트 생성자에서 처리
    - items를 같이 넘겨주는 방식
    - items없이도 할 수 있도록 개선할 것

--- app/src/main/java/com/stopstone/sunflower/view/movie/MovieAdapter.kt
+++ app/src/main/java/com/stopstone/sunflower/view/movie/MovieAdapter.kt
@@ -7,12 +7,11 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.stopstone.sunflower.data.model.Movie
 import com.stopstone.sunflower.databinding.ItemGardenBinding
 import com.stopstone.sunflower.databinding.ItemMovieBinding
-import com.stopstone.sunflower.extension.setScaleImage
+import com.stopstone.sunflower.extension.loadImage
 import com.stopstone.sunflower.view.listener.MovieClickListener
 
-class MovieAdapter(
-    private val listener: MovieClickListener,
-) : RecyclerView.Adapter<ViewHolder>() {
+
+class MovieAdapter(private val listener: MovieClickListener) : RecyclerView.Adapter<ViewHolder>() {
     private val items = mutableListOf<Movie>()
 
     override fun getItemViewType(position: Int): Int {
@@ -25,25 +24,21 @@ class MovieAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return when (viewType) {
-            0 -> MovieViewHolder(parent) { listener.onFavoriteClick(items[it]) }
-            1 -> GardenViewHolder(parent) { listener.onFavoriteClick(items[it]) }
+            0 -> MovieViewHolder(parent, items, listener) // items를 넘겨주는 방식 말고 다른 방법을 생각할 것
+            1 -> GardenViewHolder(parent, items, listener)
             else -> throw IllegalArgumentException("Invalid ViewType")
         }
     }
 
-    override fun getItemCount(): Int {
-        return items.size
-    }
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
-        with(holder) {
-            when (this) {
-                is MovieViewHolder -> bind(item, listener)
-                is GardenViewHolder -> bind(item, listener)
-            }
+        when (holder) {
+            is MovieViewHolder -> holder.bind(item)
+            is GardenViewHolder -> holder.bind(item)
         }
     }
+
+    override fun getItemCount(): Int = items.size
 
     fun updateData(updatedItems: List<Movie>) {
         items.clear()
@@ -51,71 +46,67 @@ class MovieAdapter(
         notifyDataSetChanged()
     }
 
+    // 이렇게 되면 ClickListener는 한 번 불리지만, ViewHolder 생성 시 items를 계속 넘겨줘야 한다.
     class MovieViewHolder(
-        private val parent: ViewGroup,
+        parent: ViewGroup,
+        items: List<Movie>,
+        listener: MovieClickListener,
         private val binding: ItemMovieBinding = ItemMovieBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
         ),
-        val onClick: (index: Int) -> Unit,
-    ) :
-        ViewHolder(binding.root) {
+    ) : ViewHolder(binding.root) {
         init {
+            binding.root.setOnClickListener {
+                listener.onMovieClick(items[adapterPosition])
+            }
             binding.btnFavoriteImage.setOnClickListener {
-                onClick(adapterPosition)
+                listener.onFavoriteClick(items[adapterPosition])
             }
         }
 
-        fun bind(item: Movie, listener: MovieClickListener) {
-            itemView.setOnClickListener {
-                listener.onMovieClick(item)
-            }
-
+        fun bind(item: Movie) {
             with(binding) {
                 tvPlantItemName.text = item.title
-                ivPlantItemImage.setScaleImage("${GardenViewHolder.BASE_IMAGE}${item.posterPath}")
+                ivPlantItemImage.loadImage("${BASE_IMAGE}${item.posterPath}")
                 btnFavoriteImage.isSelected = item.favorite
             }
         }
     }
 
     class GardenViewHolder(
-        private val parent: ViewGroup,
+        parent: ViewGroup,
+        items: List<Movie>,
+        listener: MovieClickListener,
         private val binding: ItemGardenBinding = ItemGardenBinding.inflate(
-            LayoutInflater.from(
-                parent.context
-            ), parent, false
+            LayoutInflater.from(parent.context),
+            parent,
+            false
         ),
-        val onClick: (index: Int) -> Unit,
-    ) :
-
-        ViewHolder(binding.root) {
+    ) : ViewHolder(binding.root) {
         init {
+            binding.root.setOnClickListener {
+                listener.onMovieClick(items[adapterPosition])
+            }
             binding.btnFavoriteImage.setOnClickListener {
-                onClick(adapterPosition)
+                listener.onFavoriteClick(items[adapterPosition])
             }
         }
 
-        fun bind(
-            item: Movie,
-            listener: MovieClickListener,
-        ) {
-            itemView.setOnClickListener {
-                listener.onMovieClick(item)
-            }
-
+        fun bind(item: Movie) {
             with(binding) {
                 tvPlantItemName.text = item.title
-                ivPlantItemImage.setScaleImage("$BASE_IMAGE${item.posterPath}")
+                ivPlantItemImage.loadImage("$BASE_IMAGE${item.posterPath}")
                 val rating = "%.1f".format(item.voteAverage)
                 tvPlantItemMovieRating.text = "Rating: $rating"
                 btnFavoriteImage.isSelected = item.favorite
             }
         }
 
-        companion object {
-            const val BASE_IMAGE = "https://image.tmdb.org/t/p/w500"
-        }
+    }
+
+    companion object {
+        const val BASE_IMAGE = "https://image.tmdb.org/t/p/w500"
     }
 }
