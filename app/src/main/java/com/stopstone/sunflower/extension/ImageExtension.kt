ddc5971fe4e41843af85794e827d9416272b03ba
stopstone <wltjr6432@naver.com>
2024-06-09 18:02:58 +0900

    중복 함수 제거

--- app/src/main/java/com/stopstone/sunflower/extension/ImageExtension.kt
+++ app/src/main/java/com/stopstone/sunflower/extension/ImageExtension.kt
@@ -4,22 +4,7 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.stopstone.sunflower.R
 
-
-// ImageView의 확장함수로 받은 이미지 resource를 Bitmap으로 변환하여 반환한다.
-//fun ImageView.load(imageUrl: Int?): Bitmap? =
-//    imageUrl?.let { BitmapFactory.decodeResource(resources, it) }!!
-//
-
-fun ImageView.setImage(imageUrl: String) {
-    Glide.with(context)
-        .load(imageUrl)
-        .placeholder(R.color.black)
-        .error(R.color.red)
-        .into(this)
-}
-
-// ImageView의 확장함수로 받은 이미지 resource를 Bitmap으로 변환하고, 사이즈를 조절하여 호출한 view에 그린다.
-fun ImageView.setScaleImage(imageUrl: String) {
+fun ImageView.loadImage(imageUrl: String) {
     Glide.with(context)
         .load(imageUrl)
         .centerCrop()
@@ -27,4 +12,3 @@ fun ImageView.setScaleImage(imageUrl: String) {
         .error(R.color.red)
         .into(this)
 }
-
