541d62b608735e91fbdbc5ffb0222c89618dddff
stopstone <wltjr6432@naver.com>
2024-06-09 17:49:23 +0900

    탭 순서 변경

--- app/src/main/java/com/stopstone/sunflower/view/MainActivity.kt
+++ app/src/main/java/com/stopstone/sunflower/view/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.stopstone.sunflower.view
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayout
+import com.stopstone.sunflower.R
 import com.stopstone.sunflower.databinding.ActivityMainBinding
 import com.stopstone.sunflower.view.garden.GardenFragment
 import com.stopstone.sunflower.view.movie.MovieFragment
@@ -12,54 +12,34 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    //    private lateinit var tabLayout: TabLayout
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-
     // 탭 전환후 복귀 시 리사이클러 뷰 초기화 현상 수정
     private val gardenFragment = GardenFragment()
     private val movieFragment = MovieFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setInitialFragment() // 초기 화면 설정
         setTabLayout()
-
-        Log.d(TAG, "$TAG onCreate")
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "$TAG onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "$TAG onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "$TAG onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "$TAG onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "$TAG onDestroy")
+    private fun setInitialFragment() {
+        supportFragmentManager.beginTransaction().apply {
+            replace(binding.containerMain.id, movieFragment)
+            commit()
+        }
     }
 
     private fun setTabLayout() {
-//        val tabLayout = findViewById(R.id.tab_main)
+        // 탭을 전환하는 방식을 더 고민해 볼 수 있음
         binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 tab?.let {
                     val fragment = when (it.position) {
-                        0 -> gardenFragment
-                        1 -> movieFragment
-                        else -> throw IllegalArgumentException("Invalid tab position")
+                        0 -> movieFragment
+                        1 -> gardenFragment
+                        else -> throw IllegalArgumentException(getString(R.string.invalid_tab_position))
                     }
                     supportFragmentManager.beginTransaction().apply {
                         replace(binding.containerMain.id, fragment as Fragment)
@@ -72,9 +52,5 @@ class MainActivity : AppCompatActivity() {
 
             override fun onTabReselected(tab: TabLayout.Tab?) {}
         })
-    }
-
-    companion object {
-        const val TAG = "MainActivity"
     }
 }
